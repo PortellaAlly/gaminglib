9be9728b2c6113d65acda7e17a447bea14ae0137
Portella <Allysonpalmeirim12@gmail.com>
2026-09-11 00:36:49 -0300

    Feat(User) | now you can create an account

---
+++ src/main/java/org/example/Domain/User/UserDAO.java
@@ -0,0 +1,30 @@
+package org.example.Domain.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class UserDAO {
+    private Connection conn;
+
+    public UserDAO(Connection connection) {
+        this.conn = connection;
+    }
+
+    public void cadastrar(String name, String email) {
+        String sql = "INSERT INTO user (name, email) VALUES (?, ?)";
+
+        try {
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, email);
+
+            preparedStatement.execute();
+            preparedStatement.close();
+            conn.close();
+        } catch(SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/main/java/org/example/Service/UserService.java
+++ src/main/java/org/example/Service/UserService.java
@@ -1,8 +1,20 @@
 package org.example.Service;
 
+import org.example.ConnectionFactory;
+import org.example.Domain.User.UserDAO;
+
+import java.sql.Connection;
+
 public class UserService {
 
-    public void cadastrar(String name, String email){
+    private ConnectionFactory connection;
 
+    public UserService(){
+        this.connection = new ConnectionFactory();
+    }
+
+    public void cadastrar(String name, String email){
+        Connection conn = connection.recuperarConexao();
+        new UserDAO(conn).cadastrar(name, email);
     }
 }
